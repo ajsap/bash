1d95f62288216cc90310644284bc650fe9f250e1
yacsap <yacsap@gmail.com>
2017-02-14 10:50:12 +1300

    Add MAILTO=""
    Add MAILTO="" to discard internal mail if the script has permission issue, e.g.: /Volumes/Macintosh HD/
    
    An example of the internal mail output:
    From andy@ANDY-MAC.local  Tue Feb 14 10:25:00 2017
    Return-Path: <andy@ANDY-MAC.local>
    X-Original-To: andy
    Delivered-To: andy@ANDY-MAC.local
    Received: by ANDY-MAC.local (Postfix, from userid 501)
            id CB6B3104315; Tue, 14 Feb 2017 10:25:00 +1300 (NZDT)
    From: andy@ANDY-MAC.local (Cron Daemon)
    To: andy@ANDY-MAC.local
    Subject: Cron <andy@ANDY-MAC> sh /Users/andy/touchdrives.sh > /Users/andy/touchdrivescron.log
    X-Cron-Env: <SHELL=/bin/sh>
    X-Cron-Env: <PATH=/usr/bin:/bin>
    X-Cron-Env: <LOGNAME=andy>
    X-Cron-Env: <USER=andy>
    X-Cron-Env: <HOME=/Users/andy>
    Message-Id: <20170213212500.CB6B3104315@ANDY-MAC.local>
    Date: Tue, 14 Feb 2017 10:25:00 +1300 (NZDT)
    Status: R
    
    touch: /Volumes/Macintosh HD/.touchdrive: Permission denied

--- touchdrives/touchdrives.sh
+++ touchdrives/touchdrives.sh
@@ -3,7 +3,9 @@
 # tested on macOS Sierra 10.12.3 (16D32)
 # schedule it on cron, e.g.:
 # ANDY-MAC:~ andy$ crontab -l
+# MAILTO=""
 # */5 * * * * sh /Users/andy/touchdrives.sh > /Users/andy/touchdrivescron.log
+
 for D in /Volumes/*; do
   if [ -d "${D}" ]; then
     echo "${D}"
