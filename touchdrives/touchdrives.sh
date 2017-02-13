0ba029c2e254b7fcdc940c0c6041a81fd733b4c3
yacsap <yacsap@gmail.com>
2017-02-14 09:48:35 +1300

    Create touchdrives.sh
    First public release

---
+++ touchdrives/touchdrives.sh
@@ -0,0 +1,12 @@
+# touchdrives.sh v1.0-macos
+# designed to keep all drives spinning on Mac
+# tested on macOS Sierra 10.12.3 (16D32)
+# schedule it on cron, e.g.:
+# ANDY-MAC:~ andy$ crontab -l
+# */5 * * * * sh /Users/andy/touchdrives.sh > /Users/andy/touchdrivescron.log
+for D in /Volumes/*; do
+  if [ -d "${D}" ]; then
+    echo "${D}"
+    touch "${D}/.touchdrive"
+  fi
+done
